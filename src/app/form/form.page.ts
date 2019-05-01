efa4b6903f2d2ca1e3f64ea0291b8082adad52e4
hernan <hernag_09@hotmail.com>
2019-05-01 17:16:23 -0300

    first commit

--- src/app/form/form.page.ts
+++ src/app/form/form.page.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { LoadingController } from '@ionic/angular';
+import {  HttpClient } from '@angular/common/http';
+import { JsonPipe } from '@angular/common';
+
 
 @Component({
   selector: 'app-form',
@@ -8,32 +11,49 @@ import { LoadingController } from '@ionic/angular';
   styleUrls: ['./form.page.scss'],
 })
 export class FormPage implements OnInit {
-
+  usuarios:{};
   name:string;
-  pass:string;
+  img:string;
    
-    constructor(public route:Router,public loading:LoadingController) { }
-  
-    
-   async submit(){
+    constructor(public route:Router,public loading:LoadingController,private http:HttpClient) { }
+
+   getView(){
+     this.http.get('http://localhost:4000/view').subscribe(data=>{
+     
+       this.usuarios = data
+        console.log(this.usuarios)
+     })
+   }
+
+
+  async sendPostRequest() {
+   
     const loading = await this.loading.create({
-      spinner:"crescent",
+      spinner: "crescent",
       message: 'wait please',
       duration: 2000
     });
     await loading.present();
-  
+
     const { role, data } = await loading.onDidDismiss();
-  
-    console.log('Loading dismissed!')
-  
-    
-  
-         this.route.navigate(["/home",{"usuario":this.name,"pasword":this.pass}])
+    let postData = {
+      "name": this.name,
+      "img": this.img
+      
     }
-  
+
+    this.http.post("http://localhost:4000/enviar", postData)
+      .subscribe(data => {
+        console.log(data)
+      }, error => {
+        console.log(error);
+      });
+      this.route.navigate(['/home'])
+      
+  }
     
     ngOnInit() {
+    this.getView()
     }
   
   }
