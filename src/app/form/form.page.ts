41df450b15a1b671923aea22ce0b7d6de3ca1e50
hernan <hernag_09@hotmail.com>
2019-05-02 01:41:42 -0300

    second commit

--- src/app/form/form.page.ts
+++ src/app/form/form.page.ts
@@ -3,6 +3,7 @@ import { Router } from '@angular/router';
 import { LoadingController } from '@ionic/angular';
 import {  HttpClient } from '@angular/common/http';
 import { JsonPipe } from '@angular/common';
+import { error } from '@angular/compiler/src/util';
 
 
 @Component({
@@ -14,17 +15,27 @@ export class FormPage implements OnInit {
   usuarios:{};
   name:string;
   img:string;
-   
+  
     constructor(public route:Router,public loading:LoadingController,private http:HttpClient) { }
 
    getView(){
      this.http.get('http://localhost:4000/view').subscribe(data=>{
-     
-       this.usuarios = data
-        console.log(this.usuarios)
+     //this.usuarios es un objeto si l oasigno como array no me deja igualarlo a la data
+     //asi como esta me lo mapea el html pero no como un array
+      this.usuarios=data
+     },error=>{
+       console.error(error);
+       
      })
    }
-
+ delete(i:number){
+   this.http.delete('http://localhost:4000/delete').subscribe(data=>{
+     console.log("deleted")
+      let a= document.createElement('a')
+       a.href="/form"
+       a.click()
+   })
+ }
 
   async sendPostRequest() {
    
